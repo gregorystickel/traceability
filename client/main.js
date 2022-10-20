587b27d744b06b86b7513231a9e4f21632019049
Greg Stickel <gregory.stickel@live.com>
2022-10-20 15:22:54 -0400

    Updated 3:22pm

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 //const axios = require("axios");
-const getRider = document.querySelector("#residents");
+const getRider = document.querySelector("#teams");
 const options = {
     method: 'GET',
     url: 'https://pro-cycling-stats.p.rapidapi.com/riders/julian-alaphilippe',
@@ -13,17 +13,18 @@ const options = {
 function chkButton () {
     
     console.log(`button clicked!`);
-    axios.get(options).then(res => {
-            console.log(res.data)
-            let { teams } = res.data
-            let div = document.createElement("div")
-            let p = document.createElement("p")
-            div.append(teams, p)
+    rollbar.log('Button clicked!');
+    // axios.get(options).then(res => {
+    //         console.log(res.data)
+    //         let { teams } = res.data
+    //         let div = document.createElement("div")
+    //         let p = document.createElement("p")
+    //         div.append(teams, p)
 
          
-        }).catch(function (error) {
-            console.error(error);
-        });
+    //     }).catch(function (error) {
+    //         console.error(error);
+    //     });
 }
 
-//getRider.addEventListener(`click`, chkButton);
+getRider.addEventListener(`click`, chkButton);
